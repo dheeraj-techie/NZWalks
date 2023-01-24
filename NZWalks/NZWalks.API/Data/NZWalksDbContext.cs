9aebe3bfb9498b5cde56b432f56b6745548636b3
dheeraj-techie <dheeraj.anchan@gmail.com>
2023-01-24 14:02:57 +0530

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domains;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options) // passing the options to the base class.
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; } // asking EF to create a table if it doesn't exist in the DB.
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
